844e4c903ec57822f7ea7a90b6da1a3323fc1f3f
JORGE SOBRINHO <roteiro.ds@gmail.com>
2026-01-16 19:01:39 -0300

    Create app.js

---
+++ frontend/app.js
@@ -0,0 +1,4 @@
+// frontend/app.js
+// Arquivo “âncora” para NÃO existir 404 no console.
+// Se depois você quiser colocar o roteador aqui, pode. Por enquanto: não quebra nada.
+console.log("[TESOURA] app.js carregado OK");
